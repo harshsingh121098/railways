d8c78ae06d3f858883708b6284531d9080daa18f
Harsh Singh <harsh121098@gmail.com>
2020-02-26 22:07:41 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+import pandas as pd
+from gtts import gTTS
+from pydub import AudioSegment
+
+def textTospeech(text,filename):
+    pass
+
+def mergeaudios(audios):  #returns pydubs audio segment
+    pass
+
+def generateSkeleton():
+    pass
+def generateannouncement(filename):
+    pass
+
+if __name__== "__main__":
+    print("genarating skeleton..")
+    generateSkeleton()
+    print("now generating announcement..")
+    generateannouncement("Railway.xlsx")
